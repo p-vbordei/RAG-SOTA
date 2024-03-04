f56489c4d5771d4bafbc100efe9709fd7c17d9a8
Vlad <bordeivlad@gmail.com>
2024-03-04 22:46:46 +0200

    pdf parser

--- pdf_parser/src/pdf_parser.py
+++ pdf_parser/src/pdf_parser.py
@@ -1,12 +1,18 @@
 # src/pdf_parser.py
-import fitz  # PyMuPDF
 import pdfplumber
 
 def parse_pdf(pdf_path):
-    with pdfplumber.open(pdf_path) as pdf:
-        full_text = ''
-        for page in pdf.pages:
-            full_text += page.extract_text() + "\n"
-    print(full_text)
+    try:
+        with pdfplumber.open(pdf_path) as pdf:
+            full_text = ''
+            for page in pdf.pages:
+                page_text = page.extract_text()
+                if page_text:  # Check if page_text is not None
+                    full_text += page_text + "\n"
+            return full_text
+    except Exception as e:
+        print(f"Error processing {pdf_path}: {e}")
+        return None
+
 
 #### end ####
